0d3260881208a2d02bef639a8953fcba9738be27
Envy Hose <envyhose@Envys-MacBook-Pro.local>
2024-01-17 23:58:38 -0600

    header merge fix

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -67,25 +67,30 @@ export default function Location(props) {
         <div>
             <Header />
             <CallBtn />
-            {/* <div id="locationHero">
-                <img alt={locationData?.name + " pool"} src={locationData?.image} id="locationHeroImg"></img>
-                <div id="buildsHeroFlex">
-                    <div id="buildsHeroLeft">
-                        <div id="buildsHeroLeftInner">
-                            <div id="buildsHeroTitle">
-                                {locationData?.name}
-                            </div>
-                            <div id="buildsHeroSub">
-                                Swimming Pool Builders in {locationData?.name}
+
+            <div id="locationHeroOuter">
+                <div id="locationHero">
+                    <img alt={locationData?.name + " pool"} src={locationData?.image} id="locationHeroImg"></img>
+                    <div id="buildsHeroFlex">
+                        <div id="buildsHeroLeft">
+                            <div id="buildsHeroLeftInner">
+                                <div id="buildsHeroTitle">
+                                    {locationData?.name}
+                                </div>
+                                <div id="buildsHeroSub">
+                                    Swimming Pool Builders in {locationData?.name}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div> */}
+            </div>
+
+
             <div id="locationArea">
-            <img alt={locationData?.name + " pool"} src={locationData?.image} id="locationHeroImg"></img>
-                <div id="locationBlur"></div>
+
                 <div id="locationAreaInner">
+
                     <div id="LocationLeft">
                         <div id="locationTitle">
                             {locationData?.name} services
@@ -167,7 +172,7 @@ export default function Location(props) {
                     </div>
                 </div>
             </div>
-            {/* <div id="locationFlagOuter">
+            <div id="locationFlagOuter">
                 <div id="locationFlag">
                     <svg id="flagIcon" xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
@@ -184,7 +189,7 @@ export default function Location(props) {
                         BOOK NOW
                     </div>
                 </div>
-            </div> */}
+            </div>
             <Footer />
         </div>
     );
